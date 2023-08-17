ce89484a09d80c6a02224caa436ab721d5b3f94a
mohamed <xKir1tox@example.com>
2023-08-16 21:30:59 -0700

    update to late

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+
+
+    /**
+     *      * main - check the code
+     *           *
+     *                * Return: Always 0.
+     *                     */
+    int main(void)
+	    {
+		            print_alphabet();
+			            return (0);
+				        }
+
